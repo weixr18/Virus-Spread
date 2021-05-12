2b13aadf7f339a3c1bb542ba4af0c09ee6abc90d
weixr18 <weixr18@mails.tsinghua.edu.cn>
2021-05-12 23:27:07 +0800

    visualization complete

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import sys
 import os.path
 
-from PyQt5.QtCore import QPoint, Qt
+from PyQt5.QtCore import QPoint, Qt, pyqtSignal, QThread
 from PyQt5.QtGui import QPainter, QPen
 from PyQt5.QtWidgets import QWidget, QApplication
 from PyQt5.uic import loadUi
@@ -13,6 +13,29 @@ from visual.show_area import ShowArea
 from visual.load_dll import load_dll
 
 
+class SimulationThread(QThread):
+    finish_signal = pyqtSignal()
+
+    def __init__(self):
+        super(QThread, self).__init__()
+        self.dll = load_dll("./bin/sim.dll")
+
+    def my_start(self, HOSPITAL_CAPACITY, P_MOVE,
+                 P_INFECT, P_VACCINATION, P_PROTECTION):
+        self.HOSPITAL_CAPACITY = HOSPITAL_CAPACITY
+        self.P_MOVE = P_MOVE
+        self.P_INFECT = P_INFECT
+        self.P_VACCINATION = P_VACCINATION
+        self.P_PROTECTION = P_PROTECTION
+        self.start()
+
+    def run(self):
+        self.dll.sim(30, self.HOSPITAL_CAPACITY, self.P_MOVE,
+                     self.P_INFECT, self.P_VACCINATION, self.P_PROTECTION)
+        self.finish_signal.emit()
+        pass
+
+
 class MainWidget(QWidget):
     def __init__(self) -> None:
         QWidget.__init__(self)
@@ -20,11 +43,13 @@ class MainWidget(QWidget):
         self.setWindowTitle('Virus Spread')
 
         self.file_name = "C500_M0.50_I0.10_V0.90_P0.50.txt"
+        self.counts = None
         self.load_data(show_day=0)
         self.day_slider.sliderMoved.connect(self.on_slider_moved)
         self.run_btn.clicked.connect(self.run)
+        self.sim_thread = SimulationThread()
+        self.sim_thread.finish_signal.connect(self.on_simulation_complete)
 
-        self.dll = load_dll("./bin/sim.dll")
         self.HOSPITAL_CAPACITY = 100
         self.P_MOVE = 0.9
         self.P_INFECT = 0.6
@@ -34,14 +59,10 @@ class MainWidget(QWidget):
         self.show()
 
     def load_data(self, show_day=0):
-        data = load_data(self.file_name)
+        data, self.counts = load_data(self.file_name)
         self.showArea.setData(data)
-        self.showArea.draw_data(show_day)
+        self.on_slider_moved(show_day)
         pass
-
-    def on_slider_moved(self, day):
-        self.showArea.draw_data(day)
-        self.day_label.setText(str(day))
 
     def run(self):
         self.HOSPITAL_CAPACITY = self.value1.value()
@@ -52,13 +73,35 @@ class MainWidget(QWidget):
         self.file_name = self.get_file_name()
         print(self.file_name)
         if not os.path.isfile('./data/'+self.file_name):
-            self.dll.sim(30, self.HOSPITAL_CAPACITY, self.P_MOVE,
-                         self.P_INFECT, self.P_VACCINATION, self.P_PROTECTION)
+            self.gb_param.setEnabled(False)
+            self.gb_run.setEnabled(False)
+            self.gb_result.setEnabled(False)
+            self.sim_thread.my_start(self.HOSPITAL_CAPACITY, self.P_MOVE,
+                                     self.P_INFECT, self.P_VACCINATION, self.P_PROTECTION)
         else:
             print("File exist.")
+            show_day = self.day_slider.value()
+            self.load_data(show_day)
 
+    def on_simulation_complete(self):
+        self.gb_param.setEnabled(True)
+        self.gb_run.setEnabled(True)
+        self.gb_result.setEnabled(True)
         show_day = self.day_slider.value()
         self.load_data(show_day)
+        pass
+
+    def on_slider_moved(self, day):
+        self.showArea.draw_data(day)
+        self.day_label.setText(str(day))
+        if self.counts is not None:
+            self.le_healthy.setText(str(self.counts[0, day]))
+            self.le_infected.setText(str(self.counts[1, day]))
+            self.le_confirmed.setText(str(self.counts[2, day]))
+            self.le_hospitalized.setText(str(self.counts[3, day]))
+            self.le_healed.setText(str(self.counts[4, day]))
+            self.le_dead.setText(str(self.counts[5, day]))
+        pass
 
     def get_file_name(self):
         str_ = "C{0}_M{1:.2f}_I{2:.2f}_V{3:.2f}_P{4:.2f}.txt".format(
--- visual/data.py
+++ visual/data.py
@@ -6,4 +6,9 @@ def load_data(file_name):
     df = pd.read_csv("./data/" + file_name, sep='\t', header=None)
     data = df.values
     data = np.reshape(data, [-1, 5000, 4])
-    return data
+    counts = []
+    for i in range(6):
+        counts.append(np.sum(data[:, :, 1] == i, axis=1))
+    counts = np.array(counts)
+    counts = counts[:, ::10]
+    return data, counts
